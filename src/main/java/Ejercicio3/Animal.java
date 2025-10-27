152ed06b58ade936c16531809fd6ac82bceafb16
DiegoColmenarez <dcolmenarezr@unicartagena.edu.co>
2025-10-27 14:19:46 -0500

    Taller completado

--- src/main/java/Ejercicio3/Animal.java
+++ src/main/java/Ejercicio3/Animal.java
@@ -1,6 +1,9 @@
 package Ejercicio3;
 
 public class Animal {
+    protected void comer() {
+        System.out.println("El animal está comiendo.");
+    }
     protected void sonido(){
         System.out.println("El animal hace un sonido");
     }
--- src/main/java/Ejercicio3/Gato.java
+++ src/main/java/Ejercicio3/Gato.java
@@ -5,8 +5,11 @@ public class Gato extends Animal {
     protected void sonido(){
         System.out.println("Miau~");
     }
+    @Override
+    protected void comer() {
+        System.out.println("El animal está comiendo.");
+    }
     protected void dormir(){
         System.out.println("ZZZZzzzz...");
     }
-
 }
